50ea80757898cb9b3e9d05ea00589ace13126ba3
Ania <ania.wargocka98@gmail.com>
2020-08-31 15:48:41 +0200

    Init commit

--- SpringORM/src/main/java/com/example/SpringORM/ClientRepo.java
+++ SpringORM/src/main/java/com/example/SpringORM/ClientRepo.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepo extends CrudRepository<Client, Long>{
-	List<Client> findByName(String name);
+	//List<Client> findByName(String name);
 	Client findById(long id);
 }
--- SpringORM/src/main/java/com/example/SpringORM/CourtReservationRepo.java
+++ SpringORM/src/main/java/com/example/SpringORM/CourtReservationRepo.java
@@ -1,12 +1,17 @@
 package com.example.SpringORM;
 
-import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+/*
 public interface CourtReservationRepo extends CrudRepository< CourtReservation, Long>{
 	//List< CourtReservation> findByNumber(int nr_kortu);
 	CourtReservation findById(long id);
+}
+*/
+public interface CourtReservationRepo extends JpaRepository<CourtReservation, Long>{
+
 }
